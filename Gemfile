e97f009a13bb92699aa8b14a3ce14ab34a4fd3a4
Thibaud Vuitton <vuittont@gmail.com>
2018-02-16 11:25:53 +0100

    routes rerouted

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'jquery-rails' # Add this line if you use Rails 5.1
 gem 'figaro'
-
+gem 'cloudinary'
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'cocktails#index'
 resources :cocktails, only: [:create, :index, :new, :show] do
 resources :doses, only: [:create, :new]
   end
